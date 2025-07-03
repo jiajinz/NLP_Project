5113ed7a9e16d1012c5bdd6ee125c57184fddc65
Curtis Neiderer <curtis.neiderer@gmail.com>
2025-07-02 23:30:01 -0400

    updated preprocessing imports

--- src/emolex/preprocessing.py
+++ src/emolex/preprocessing.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from transformers import BertTokenizer, BertTokenizerFast, DistilbertTokenizer, DistilbertTokenizerFast
+from transformers import BertTokenizer, BertTokenizerFast, DistilBertTokenizer, DistilBertTokenizerFast
 from datasets import Dataset
 
 # NLTK Downloads
